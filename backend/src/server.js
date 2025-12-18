790de3e2d116e02c81429afb641364b3bd2d9bdc
annascodes <itsanascode@gmail.com>
2025-12-18 16:21:38 +0500

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import authRoutes from './routes/auth.routes.js'
+ 
+const app = express();
+
+
+app.use('/api/auth', authRoutes)
+
+
+app.listen(3000, ()=>{
+    console.log(`-- server is running --`.toUpperCase() )
+})
